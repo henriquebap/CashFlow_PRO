611f8bb16ecab7e4a29479d05c2c776c3836bd07
Henriquebap <henriquebaptista2003@gmail.com>
2024-03-01 11:53:29 -0300

    Criando os endpoints

--- src/main/java/br/com/fiap/cashflowpro/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/cashflowpro/controller/CategoriaController.java
@@ -1,16 +1,57 @@
 package br.com.fiap.cashflowpro.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.fiap.cashflowpro.model.Categoria;
+
 @Controller
 public class CategoriaController {
-    
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    List<Categoria> repository = new ArrayList<>();
+
     @RequestMapping(method = RequestMethod.GET, path = "/categoria", produces = "application/json")
     @ResponseBody
-    public String index(){
-        return "Lista";
+    public List<Categoria> index() {
+        return repository;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/categoria")
+    @ResponseBody
+    // @ResponseStatus(code = HttpsStatus.CREATED)
+    public ResponseEntity<Categoria> create(@RequestBody Categoria categoria) {
+        log.info("Cadastrando Categoria: {}", categoria);
+        repository.add(categoria);
+        return ResponseEntity.status(201).body(categoria);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/categoria/{id}")
+    @ResponseBody
+    public ResponseEntity<Categoria> get(@PathVariable Long id) {
+        log.info("Buscando Categoria com id: {}", id);
+
+        // Stream
+        var categoria = repository
+                .stream()
+                .filter(c -> c.id().equals(id))
+                .findFirst();
+
+        if (categoria.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        }
+
+        return ResponseEntity.status(200).body(categoria.get());
     }
 }
---
+++ src/main/java/br/com/fiap/cashflowpro/model/Categoria.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cashflowpro.model;
+
+import java.util.Random;
+
+public record Categoria(Long id, String nome, String icone) {
+
+    public Categoria(Long id, String nome, String icone) {
+        var key = (id != null) ? id : Math.abs(new Random().nextLong());
+        this.id = key;
+        this.icone = icone;
+        this.nome = nome;
+    }
+}
